bca24ce3d93e46cea51384cb18b4a912f1fdfc98
Dhruval Variya <31678448+dhruvalv@users.noreply.github.com>
2026-07-22 19:45:56 -0700

    Add dependency extraction tests

--- engine/src/test/java/com/architectureoslabs/engine/parser/JavaSourceParserTest.java
+++ engine/src/test/java/com/architectureoslabs/engine/parser/JavaSourceParserTest.java
@@ -1,13 +1,12 @@
 package com.architectureoslabs.engine.parser;
 
 
+import com.architectureoslabs.engine.model.ParsedJavaFile;
 import com.architectureoslabs.engine.model.SoftwareComponent;
 
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
-
 
 
 /**
@@ -16,14 +15,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class JavaSourceParserTest {
 
 
-
     @Test
     void shouldExtractClassNameFromJavaSource() {
 
 
         JavaSourceParser parser =
                 new JavaSourceParser();
-
 
 
         String sourceCode = """
@@ -35,14 +32,15 @@ public class JavaSourceParserTest {
                 """;
 
 
-
-        SoftwareComponent component =
+        ParsedJavaFile parsedFile =
                 parser.parse(sourceCode);
 
 
+        SoftwareComponent component =
+                parsedFile.getComponent();
+
 
         assertNotNull(component);
-
 
 
         assertEquals(
@@ -51,10 +49,61 @@ public class JavaSourceParserTest {
         );
 
 
-
         assertEquals(
                 "class",
                 component.getType()
+        );
+
+    }
+
+
+    @Test
+    void shouldExtractImportsAsDependencies() {
+
+
+        JavaSourceParser parser =
+                new JavaSourceParser();
+
+
+        String sourceCode = """
+
+                import com.company.repository.PaymentRepository;
+                import com.company.user.UserService;
+
+
+                public class PaymentService {
+
+                }
+
+                """;
+
+
+        ParsedJavaFile parsedFile =
+                parser.parse(sourceCode);
+
+
+        assertNotNull(parsedFile);
+
+
+        assertEquals(
+                2,
+                parsedFile.getDependencies().size()
+        );
+
+
+        assertEquals(
+                "PaymentRepository",
+                parsedFile.getDependencies()
+                        .get(0)
+                        .getTarget()
+        );
+
+
+        assertEquals(
+                "UserService",
+                parsedFile.getDependencies()
+                        .get(1)
+                        .getTarget()
         );
 
     }
