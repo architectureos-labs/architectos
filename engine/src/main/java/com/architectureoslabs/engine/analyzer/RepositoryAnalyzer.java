df282a2f6e4856897e823e17bb33b5d35dbefcfe
Dhruval Variya <31678448+dhruvalv@users.noreply.github.com>
2026-07-23 18:24:01 -0700

    Update RepositoryAnalyzer.java

--- engine/src/main/java/com/architectureoslabs/engine/analyzer/RepositoryAnalyzer.java
+++ engine/src/main/java/com/architectureoslabs/engine/analyzer/RepositoryAnalyzer.java
@@ -1,51 +1,46 @@
 package com.architectureoslabs.engine.analyzer;
 
-
+import com.architectureoslabs.engine.model.ArchitectureGraph;
+import com.architectureoslabs.engine.model.Dependency;
 import com.architectureoslabs.engine.model.ParsedJavaFile;
 import com.architectureoslabs.engine.parser.JavaSourceParser;
 
-
-
 /**
- * Analyzes software repositories and extracts architecture information.
- *
- * This class coordinates:
- *
- * - Source parsing
- * - Component discovery
- * - Dependency extraction
+ * Coordinates repository analysis and produces an architecture graph.
  */
 public class RepositoryAnalyzer {
 
-
     private final JavaSourceParser parser;
 
-
-
     public RepositoryAnalyzer() {
-
-        this.parser =
-                new JavaSourceParser();
-
+        this.parser = new JavaSourceParser();
     }
 
-
-
     /**
-     * Analyze Java source content.
+     * Analyze Java source code and produce an architecture graph.
      *
-     * @param sourceCode Java source code
-     * @return parsed architecture information
+     * Future versions will:
+     * - Scan directories
+     * - Parse every Java file
+     * - Merge all parsed files into one graph
      */
-    public ParsedJavaFile analyze(
-            String sourceCode
-    ) {
+    public ArchitectureGraph analyze(String sourceCode) {
 
+        ParsedJavaFile parsedFile =
+                parser.parse(sourceCode);
 
-        return parser.parse(
-                sourceCode
+        ArchitectureGraph graph =
+                new ArchitectureGraph();
+
+        graph.addComponent(
+                parsedFile.getComponent()
         );
 
+        for (Dependency dependency : parsedFile.getDependencies()) {
+            graph.addDependency(dependency);
+        }
+
+        return graph;
     }
 
 }
